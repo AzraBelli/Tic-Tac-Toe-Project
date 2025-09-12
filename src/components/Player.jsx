6746a7018fca8af368b4465a1bca345e360c2169
AzraBELLI <77413791+AzraNurMISIRBELLI@users.noreply.github.com>
2025-09-12 00:38:31 -0500

    Lifting State Up

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
-    const [playerName,setPlayerNAme]=useState(initialName);
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerNAme] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    function handleChange(event)
-    {        
+    function handleChange(event) {
         setPlayerNAme(event.target.value);
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
-    
+
     if (isEditing) {
         editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
-        
+
     }
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
