0ccc813fe52b80e63782700f4bc1f84189ffc53e
AzraBELLI <77413791+AzraNurMISIRBELLI@users.noreply.github.com>
2025-09-30 22:37:27 -0500

    Added Props and Log

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,7 +5,15 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     // function handleSelectSquare(rowIndex, colIndex,) {
@@ -18,15 +26,19 @@ export default function GameBoard({ onSelectSquare }) {
     //     onSelectSquare();
     // }
 
-    return <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-            <ol>
-                {row.map((playerSymbol, colIndex) => (
-                    <li key={colIndex}>
-                        <button onClick={onSelectSquare}>{playerSymbol}</button>
-                    </li>))}
-            </ol>
-
-        </li>)}
-    </ol>
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
 }
